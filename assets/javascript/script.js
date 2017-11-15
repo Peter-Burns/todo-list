47f7e479d983d8110d48b135ab8f345bf9946974
Peter Burns <peterburnshicks@gmail.com>
2017-11-15 04:05:46 -0500

    allcheck now correctly applies strikethrough

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 var status;
-var undoHolder;
+var undoHolder;//make it an array with position
 function updateElements() {
-    $('#itemsLeft').text(`${$('.todoItem').length} items left`)
+    $('#itemsLeft').text(`${$('.todoItem').length} items left`);
     if ($('.todoItem').text()) {
         $('.todoFunctions').css('display', 'block');
     }
@@ -34,7 +34,7 @@ $('#inputLine').on('keyup', function (event) {
     if (event.key === 'Enter' && $('#inputLine').val()) {
         var newTodo = $(`<div class="todoItem">${$('#inputLine').val()}</div>`);
         $('#inputLine').val('');
-        $('#todoHolder').after(newTodo);
+        $('#todoHolder').prepend(newTodo);
         newTodo.prepend($('<input class="checkLeft check" type="checkBox">'));
         newTodo.append($('<button class="deleteItem">X</button>'));
         updateElements();
@@ -56,11 +56,17 @@ $('body').on('change', '.checkLeft', function () {
     }
     else {
         $('#allCheck').prop('checked', false);
-        $(this).parent().css('text-decoration', 'none')
+        $(this).parent().css('text-decoration', 'none');
     }
 });
 $('#allCheck').on('change', function () {
     $('input:checkbox').not(this).prop('checked', this.checked);
+    if(this.checked){
+        $('#todoHolder').children().css('text-decoration', 'line-through');
+    }
+    else{
+        $('#todoHolder').children().css('text-decoration', 'none');
+    }
     updateClear();
 });
 $('#clearCompleted').on('click', function () {
@@ -73,7 +79,7 @@ $('#clearCompleted').on('click', function () {
 });
 $('.undo').on('click', function () {
     $('.undo').hide();
-    $('#todoHolder').after(undoHolder);
+    $('#todoHolder').prepend(undoHolder);
     updateClear();
     updateElements();
 });
